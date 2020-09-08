ff170f810679240e4ec877b2c3a25aebff13f6d0
Шияфетдинов Даниль <onkain22@gmail.com>
2020-09-08 14:37:24 +0300

    f

--- CreateStruct/Program.cs
+++ CreateStruct/Program.cs
@@ -1,21 +1,26 @@
 ﻿using System;
 
-namespace practice01_1
+namespace practice01_2
 {
   struct Person
   {
     public string firstName;
     public string lastName;
     public int age;
+    public Genders gender;
+    public enum Genders : int { Male, Female };
 
-    public Person(string _firstName, string _lastName, int _age)
+
+    public Person(string _firstName, string _lastName, int _age, Genders _gender)
     {
       firstName = _firstName;
-      lastName = _lastName; age = _age;
+      lastName = _lastName; 
+      age = _age;
+      gender = _gender;
     }
     public override string ToString()
     {
-      return firstName + " " + lastName + ", age " + age;
+      return firstName + " " + lastName + " (" + gender + "), age " + age;
     }
   }
 
@@ -23,8 +28,8 @@ namespace practice01_1
   {
     static void Main(string[] args)
     {
-      Person p = new Person("Tony", "Allen", 32);
-      Console.WriteLine(p);
+      Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+      Console.WriteLine(p.ToString());
     }
   }
 }
